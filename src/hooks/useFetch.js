ae5415d12dbb46cf32e74611fe42bd52b5464ecc
Kilian <dankestmirror@hotmail.es>
2026-04-17 16:45:12 +0100

    ite 4

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -8,18 +8,24 @@ export function useFetch(url){
     const [error, setError] = useState(null)
 
     useEffect(() => {
+        const controller = new AbortController();
+
         const fetchData = async () => {
             try {
                 setLoading(true); //Cuando empezamos la carga
-                const responsive = await axios.get(url)
+                const responsive = await axios.get(url, { signal : controller.signal});
                 setData(responsive.data) //Guarda los datos en data
             } catch (error) {
+                if(axios.isCancel(error)) console.log('Cancelada', error.message)
                 setError(error) //Se guarda el error 
             }finally{
                 setLoading(false)
             }
         };
         fetchData();
+        return () =>{
+            controller.abort();
+        }
     }, [url]); //Si llega a cambiar la url la vuelve a pedir el hook
     return {data , loading , error}
 }
